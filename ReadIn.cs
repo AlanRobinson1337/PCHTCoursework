f95e66921df51ef16e292138f234bc547ce10c0e
alanr <2576847R@student.gla.ac.uk>
2023-11-08 16:03:24 +0000

    Added readInCSV for data class

--- ReadIn.cs
+++ ReadIn.cs
@@ -33,6 +33,36 @@ namespace PCHTCoursework
                 return result;
             }
         }
+        //Readin CSV for DataClass
+        public static List<double> readInCSVDataClass(string csvFilePath)
+        {
+            using (var reader = new StreamReader(csvFilePath))
+            {
+                List<string> csvValues = new List<string>();
+                List<double> result = new List<double>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    csvValues.Add(values[0]);
+                    csvValues.Add(values[1]);
+                    csvValues.Add(values[2]);
+
+                }
+                foreach (var value in csvValues)
+                {
+                    try
+                    {
+                        result.Add(double.Parse(value));
+                    }
+                    catch
+                    {
+                    }
+                }
+                return result;
+            }
+        }
         public static List<double> ReadInPupilDiamater(string csvFilePath)
         {
             List<double> list = readInCSV(csvFilePath, 0);
