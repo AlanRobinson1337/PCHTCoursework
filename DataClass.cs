f1d17e2b4f991f74f56728f8f7e404834cf5b8b0
alanr <2576847R@student.gla.ac.uk>
2023-11-08 15:11:43 +0000

    added DataClass class to make data comparions easier

---
+++ DataClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCHTCoursework
+{
+    internal class DataClass
+    {
+        private String name;
+        private String emotion;
+        private List<double> pupilDilation;
+        private List<double> fixationDuration;
+        private List<int> reigonOfInterest;
+
+        public DataClass(string name, string emotion, List<double> pupilDilation, List<double> fixationDuration, List<int> reigonOfInterest)
+        {
+            this.name = name;
+            this.emotion = emotion;
+            this.pupilDilation = pupilDilation;
+            this.fixationDuration = fixationDuration;
+            this.reigonOfInterest = reigonOfInterest;
+        }
+
+    }
+}
